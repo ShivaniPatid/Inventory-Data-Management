abb6d3e05275e73287d0db8ce2040cfefee20a6b
ShivaniPatid <shivanipatidar712@gmail.com>
2022-10-02 20:40:29 +0530

    [Shivani] Inventory Data Management

---
+++ InventoryDataManagement/FetchForJsonInventoryData.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+
+namespace InventoryDataManagement
+{
+    internal class FetchForJsonInventoryData
+    {
+        public Inventory Read(string path)
+        {
+            using(StreamReader file=new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Inventory>(json);
+
+                }
+                catch(Exception)
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ InventoryDataManagement/Inventory.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryDataManagement
+{
+    public class Inventory
+    {
+        public List<TypeOfRice> typeOfRice;
+        public List<TypeOfPulses> typeOfPulses;
+        public List<TypeOfWheat> typeOfWheat;
+    }
+
+    public class TypeOfRice
+    {
+        public string name { get; set; }
+        public int weight { get; set; }
+        public int price { get; set; }
+
+    }
+
+    public class TypeOfPulses
+    {
+        public string name { get; set; }
+        public int weight { get; set; }
+        public int price { get; set; }
+
+    }
+
+    public class TypeOfWheat
+    {
+        public string name { get; set; }
+        public int weight { get; set; }
+        public int price { get; set; }
+
+    }
+}
